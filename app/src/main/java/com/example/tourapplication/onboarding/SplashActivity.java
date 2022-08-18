746aff1528733d79d98616c1879b8796921a3012
Bayanalharbi1 <110800541+Bayanalharbi1@users.noreply.github.com>
2022-08-18 17:12:01 +0300

    Influencer update 3

---
+++ app/src/main/java/com/example/tourapplication/onboarding/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.example.tourapplication.onboarding;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import com.example.tourapplication.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_splash);
+        getSupportActionBar().hide();
+
+        final Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, OnBoardActivity.class));
+                finish();
+            }
+        }, 3000);
+    }
+}
